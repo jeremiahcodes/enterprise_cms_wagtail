f654827a9f6fb6bf7e371120678a019afb375d23
Jeremiah E. Wright <jeremiahewdev@gmail.com>
2025-09-07 15:37:45 -0400

    Add initial setup for Wagtail (#1)

---
+++ cms/enterprise-cms/cms/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-j%c%f2#_e4s3g#s&4=9sztcsem6zpt#!e%!8yznb6xx!yin@)%"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
